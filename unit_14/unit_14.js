147118813a744941f0afadb14a201d57d09ec822
Cedram <bradar748@gmail.com>
2023-08-31 16:10:55 +0300

    иконка

--- unit_14/unit_14.js
+++ unit_14/unit_14.js
@@ -1,5 +1,12 @@
 const apiKey = "46f4158489f8060c1bf148c2bcb9979a";
 const url = "https://api.openweathermap.org/data/2.5/weather";
+const cities = [
+  { value: "2643743", name: "London" },
+  { value: "2968815", name: "Paris" },
+  { value: "703448", name: "Kyiv" },
+  { value: "745044", name: "Istanbul" },
+  { value: "3451189", name: "Rio de Janeiro" },
+];
 
 function getWeather() {
   const cityId = document.querySelector("#city").value;
@@ -14,16 +21,33 @@ function showWeather(data) {
   const temperatureElement = document.getElementById("temperature");
   const weatherElement = document.getElementById("weather");
   const windSpeedElement = document.getElementById("wind-speed");
-  const humidityElement = document.getElementById("humidity");
+  const windDirectionElement = document.getElementById("wind-direction");
+  const weatherIconElement = document.getElementById("weather-icon");
   const pressureElement = document.getElementById("pressure");
 
   cityNameElement.textContent = data.name;
   temperatureElement.textContent = data.main.temp;
   weatherElement.textContent = data.weather[0].description;
   windSpeedElement.textContent = data.wind.speed;
-  humidityElement.textContent = data.main.humidity;
+  windDirectionElement.textContent = data.wind.deg;
+
+  const weatherIconId = data.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/w/${weatherIconId}.png`;
+  weatherIconElement.src = iconUrl;
+
   pressureElement.textContent = data.main.pressure;
 }
 
-document.querySelector("#city").addEventListener("change", getWeather);
-getWeather();
+document.addEventListener("DOMContentLoaded", function () {
+  const selectElement = document.getElementById("city");
+
+  cities.forEach((city) => {
+    const optionElement = document.createElement("option");
+    optionElement.value = city.value;
+    optionElement.textContent = city.name;
+    selectElement.appendChild(optionElement);
+  });
+
+  document.querySelector("#city").addEventListener("change", getWeather);
+  getWeather();
+});
